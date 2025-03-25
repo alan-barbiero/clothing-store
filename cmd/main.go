d2feba1ec59e959eaf8f25e4e0e686f64544fe02
alan <barbiero.iper@gmail.com>
2025-03-25 00:09:37 -0300

    Update database connection and manage dependencies - Modified 'cmd/main.go' to adjust the database connection, including improvements in the configuration. - Updated 'go.mod' to ensure all required dependencies are properly defined. - Added 'go.sum' to ensure integrity and consistency of dependencies.

--- cmd/main.go
+++ cmd/main.go
@@ -1,10 +1,36 @@
 package main
 
-import "log"
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/jackc/pgx/v4"
+	"github.com/joho/godotenv"
+)
 
 func main() {
-	err := db.Connect()
+	err := godotenv.Load("../.env")
 	if err != nil {
-		log.Fatal("Error to connect with database")
+		log.Fatal("error loading .env file")
 	}
+
+	dbUser := os.Getenv("DB_USER")
+	dbPassword := os.Getenv("DB_PASSWORD")
+	dbName := os.Getenv("DB_NAME")
+	dbHost := os.Getenv("DB_HOST")
+	dbPort := os.Getenv("DB_PORT")
+
+	connString := fmt.Sprintf("postgres://%s:%s@%s:%s/%s", dbUser, dbPassword, dbHost, dbPort, dbName)
+
+	conn, err := pgx.Connect(context.Background(), connString)
+	if err != nil {
+		log.Fatal("error connecting database:", err)
+	} else {
+		fmt.Println("connected to database successfully")
+	}
+
+	defer conn.Close(context.Background())
+
 }
